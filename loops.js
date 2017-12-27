81bf6d0ec63368e1d6b258eb2f4354cfd3e68fe5
michael lynn <michael_lynn@comcast.net>
2017-12-27 20:53:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,34 @@
-var coinFace = Math.floor(Math.random() * 2);
-
-while(coinFace === 0){
-	console.log("Heads! Flipping again...");
-	var coinFace = Math.floor(Math.random() * 2);
+function forLoop(array){
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+  }
 }
-console.log("Tails! Done flipping.");
+return array
+}
+
+var sample_arr = [];
+console.log(forLoop(sample_arr));
+
+
+function whileLoop(n){
+  while(n>0)
+  {console.log(n--)}
+  return "done"
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+function doWhileLoop(array){
+do {
+  array.pop()
+} while (array.length>0 && maybeTrue())
+return array
+}
+
+do_while_arr = ['a','b','c'];
+console.log(doWhileLoop(do_while_arr))
